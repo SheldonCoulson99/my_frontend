0f2126ec85b424e5a594fa4f48cd45a89572fa12
Sheldon Coulson <45391523+SheldonCoulson99@users.noreply.github.com>
2020-07-31 12:45:19 +0800

    Add files via upload

---
+++ frontend/day2/rollcnt.js
@@ -0,0 +1,36 @@
+new Vue({
+    el: '#app',
+    data: {
+        count: 0,
+        countBefore: -1,
+        countAfter: 1,
+        isBefore: false,
+        isAfter: false,
+    },
+    computed: {
+        isAnimating() {
+            return this.isBefore || this.isAfter
+        }
+    },
+    methods: {
+        subtract() {
+            this.isBefore = true
+            setTimeout(() => {
+                this.count--
+                this.countBefore = this.count - 1
+                this.countAfter = this.count + 1
+                this.isBefore = false
+            }, 200)
+        },
+        add() {
+            this.isAfter = true
+            setTimeout(() => {
+                this.count++
+                this.countBefore = this.count - 1
+                this.countAfter = this.count + 1
+                this.isAfter = false
+            }, 200)
+        }
+    }
+
+})
